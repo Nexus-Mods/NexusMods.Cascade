717ddd5a4cbfdb4fae8a9ef3d726a534fb1839c2
Timothy Baldridge <tbaldridge@gmail.com>
2025-04-14 06:47:17 -0600

    Bit of documentation

--- src/NexusMods.Cascade/Abstractions/FlowDescription.cs
+++ src/NexusMods.Cascade/Abstractions/FlowDescription.cs
@@ -4,10 +4,15 @@ namespace NexusMods.Cascade.Abstractions;
 
 public record FlowDescription
 {
+    /// <summary>
+    /// Debug information about the flow, such as the file and line number where it was created. In release mode this c
+    /// can easily be stripped out with feature flags.
+    /// </summary>
     public DebugInfo? DebugInfo { get; init; } = null;
 
-    private object? StateFactory { get; init; } = null;
-
+    /// <summary>
+    /// Called to create an initial UserState when the flow is instantiated. If it is not set, no state will be created.
+    /// </summary>
     public Func<object>? InitFn { get; init; } = null;
 
     /// <summary>
@@ -17,6 +22,11 @@ public record FlowDescription
     /// </summary>
     public Func<Node, object>? StateFn { get; init; } = null;
 
+    /// <summary>
+    /// References to upstream flows that this flow depends on. There should be a ReducerFn for each of these flows. When
+    /// data comes in from the upstream flow, the corresponding ReducerFn will be called with the data and the current
+    /// UserState of the node.
+    /// </summary>
     public FlowDescription[] UpstreamFlows { get; init; } = [];
 
     public Node.ReducerFn[] Reducers { get; init; } = [];
